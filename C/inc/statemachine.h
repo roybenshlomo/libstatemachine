8520634fc79fe8b537773a81033a36bd1f7a9a0f
Roy Ben Shlomo <roy.benshlomo@gmail.com>
2019-09-09 20:45:56 +0300

    statemachine: running clang-format

--- C/inc/statemachine.h
+++ C/inc/statemachine.h
@@ -9,41 +9,43 @@ typedef void (*event_handler)(void *);
 /*
  * definition of a single state machine entry
  */
-typedef struct statemachine_entry_t
-{
-    unsigned int next_state;
-    event_handler handler;
+typedef struct statemachine_entry_t {
+	unsigned int next_state;
+	event_handler handler;
 } statemachine_entry_t;
 
 /*
  * definition of state machine encapsulating structure
  */
-typedef struct statemachine_t
-{
-    unsigned int current_state;
-    statemachine_entry_t *states;
-    unsigned int max_states;
-    unsigned int max_events;
+typedef struct statemachine_t {
+	unsigned int current_state;
+	statemachine_entry_t *states;
+	unsigned int max_states;
+	unsigned int max_events;
 } statemachine_t;
 
 /*
  * create a state machine instance
  */
-statemachine_t * create_statemachine(unsigned int num_states, unsigned int num_events, unsigned int initial_state);
+statemachine_t *create_statemachine(unsigned int num_states,
+				    unsigned int num_events,
+				    unsigned int initial_state);
 
 /*
  * add an event handler to the state machine
  */
-void add_event_handler(statemachine_t * statemachine, unsigned int state, unsigned int event, event_handler handler, unsigned int next_state);
+void add_event_handler(statemachine_t *statemachine, unsigned int state,
+		       unsigned int event, event_handler handler,
+		       unsigned int next_state);
 
 /*
  * handle an event (will run the actual handler of the event)
  */
-void handle_event(statemachine_t * statemachine, unsigned int event, void * args);
+void handle_event(statemachine_t *statemachine, unsigned int event, void *args);
 
 /*
  * destroy state machine and deallocate all allocated memory
  */
-void destory_statemachine(statemachine_t * statemachine);
+void destory_statemachine(statemachine_t *statemachine);
 
 #endif //_STATE_MACHINE_H_
--- C/src/statemachine.c
+++ C/src/statemachine.c
@@ -4,84 +4,96 @@
 /*
  * get entry at the appropriate field in the [state, event] matrix
  */
-statemachine_entry_t * get_entry(statemachine_t * statemachine, unsigned int state, unsigned int event)
+statemachine_entry_t *get_entry(statemachine_t *statemachine,
+				unsigned int state, unsigned int event)
 {
-    return (statemachine_entry_t *)(statemachine->states + (state*event+event));
+	return (statemachine_entry_t *)(statemachine->states +
+					(state * event + event));
 }
 
 /*
  * create a state machine instance
  */
-statemachine_t * create_statemachine(unsigned int num_states, unsigned int num_events, unsigned int initial_state)
+statemachine_t *create_statemachine(unsigned int num_states,
+				    unsigned int num_events,
+				    unsigned int initial_state)
 {
-    // allocate a new state machine
-    statemachine_t * statemachine = (statemachine_t *)malloc(sizeof(statemachine_t));
+	// allocate a new state machine
+	statemachine_t *statemachine =
+		(statemachine_t *)malloc(sizeof(statemachine_t));
 
-    // check that allocation succeeded
-    if (statemachine) {
-        // set initial state
-        statemachine->current_state = initial_state;
+	// check that allocation succeeded
+	if (statemachine) {
+		// set initial state
+		statemachine->current_state = initial_state;
 
-        // set maximal values
-        statemachine->max_events = num_events;
-        statemachine->max_states = num_states;
+		// set maximal values
+		statemachine->max_events = num_events;
+		statemachine->max_states = num_states;
 
-        // allocate the data structures for the statemachines
-        statemachine->states = (statemachine_entry_t *)malloc(sizeof(statemachine_entry_t) * num_states * num_events);
+		// allocate the data structures for the statemachines
+		statemachine->states = (statemachine_entry_t *)malloc(
+			sizeof(statemachine_entry_t) * num_states * num_events);
 
-        for (unsigned int state = 0; state < num_states; state++) {
-            for (unsigned int event = 0; event < num_events; event++) {
-                statemachine_entry_t * current_entry = get_entry(statemachine, state, event);
-                current_entry->handler = 0;
-                current_entry->next_state = 0;
-            }
-        }
-    }
+		for (unsigned int state = 0; state < num_states; state++) {
+			for (unsigned int event = 0; event < num_events;
+			     event++) {
+				statemachine_entry_t *current_entry =
+					get_entry(statemachine, state, event);
+				current_entry->handler = 0;
+				current_entry->next_state = 0;
+			}
+		}
+	}
 
-    return statemachine;
+	return statemachine;
 }
 
 /*
  * destroy state machine and deallocate all allocated memory
  */
-void destory_statemachine(statemachine_t * statemachine)
+void destory_statemachine(statemachine_t *statemachine)
 {
-    free(statemachine->states);
-    free(statemachine);
+	free(statemachine->states);
+	free(statemachine);
 }
 
 /*
  * add an event handler to the state machine
  */
-void add_event_handler(statemachine_t * statemachine, unsigned int state, unsigned int event, event_handler handler, unsigned int next_state)
+void add_event_handler(statemachine_t *statemachine, unsigned int state,
+		       unsigned int event, event_handler handler,
+		       unsigned int next_state)
 {
-    // check that the state and event are not out of range
-    if (state >= statemachine->max_states || event >=statemachine->max_events) {
-        return;
-    }
+	// check that the state and event are not out of range
+	if (state >= statemachine->max_states ||
+	    event >= statemachine->max_events) {
+		return;
+	}
 
-    statemachine_entry_t * entry = get_entry(statemachine, state, event);
+	statemachine_entry_t *entry = get_entry(statemachine, state, event);
 
-    if (entry) {
-        entry->handler = handler;
-        entry->next_state = next_state;
-    }
+	if (entry) {
+		entry->handler = handler;
+		entry->next_state = next_state;
+	}
 }
 
 /*
  * handle an event (will run the actual handler of the event)
  */
-void handle_event(statemachine_t * statemachine, unsigned int event, void * args)
+void handle_event(statemachine_t *statemachine, unsigned int event, void *args)
 {
-    // check that event is within allowed range
-    if (event >= statemachine->max_events) {
-        return;
-    }
+	// check that event is within allowed range
+	if (event >= statemachine->max_events) {
+		return;
+	}
 
-    statemachine_entry_t * entry = get_entry(statemachine, statemachine->current_state, event);
-    
-    if (entry) {
-        entry->handler(args);
-        statemachine->current_state = entry->next_state;
-    }
+	statemachine_entry_t *entry =
+		get_entry(statemachine, statemachine->current_state, event);
+
+	if (entry) {
+		entry->handler(args);
+		statemachine->current_state = entry->next_state;
+	}
 }
